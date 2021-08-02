b95c495d9155fc43d2d33dc3deca52c01f18e28c
bengallagher <bgallag5@gmail.com>
2021-08-02 14:52:52 -0400

    populated table

---
+++ db/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE candidates (
+    id  INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(45) NOT NULL,
+    last_name VARCHAR(45) NOT NULL,
+industry_connected BOOLEAN NOT NULL
+);
